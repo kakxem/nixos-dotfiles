353a83cc3cc47a5f241b17058d7a482caf48ff4c
kakxem <80788785+kakxem@users.noreply.github.com>
2024-01-13 18:01:16 +0100

    fix: cache implementation

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
   
   # the nixConfig here only affects the flake itself, not the system configuration!
   nixConfig = {
-    # override the default substituters
-    substituters = [
-      "https://cache.nixos.org"
-
+    # will be appended to the system-level substituters
+    extra-substituters = [
       # nix community's cache server
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+
+    # will be appended to the system-level trusted-public-keys
+    extra-trusted-public-keys = [
       # nix community's cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
@@ -66,6 +66,18 @@
 
             {
 	      nix.settings.trusted-users = [ user ];
+
+              # the system-level substituers & trusted-public-keys
+              nix.settings = {
+                substituters = [
+                  "https://cache.nixos.org"
+                ];
+
+                trusted-public-keys = [
+                  # the default public key of cache.nixos.org, it's built-in, no need to add it here
+                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                ];
+	      };
 	    }
           ];
         };
