c544837a52db35f854a5e873b31eae4b46ab58b7
kakxem <80788785+kakxem@users.noreply.github.com>
2024-01-29 18:16:39 +0100

    fix: exec-once programs

--- modules/desktops/hyprland/home.nix
+++ modules/desktops/hyprland/home.nix
@@ -26,8 +26,10 @@ let
     # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
     # Execute your favorite apps at launch
-    # exec-once = waybar & hyprpaper & firefox
-    exec-once = hyprpaper
+    exec-once = hyprpaper &
+    exec-once = firefox &
+    exec-once = telegram-desktop &
+    exec-once = vencorddesktop &
 
     # Source a file (multi-file configs)
     # source = ~/.config/hypr/myColors.conf
