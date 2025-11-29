002cdff60ee719141b49dfd499f4ece643209d14
kakxem <80788785+kakxem@users.noreply.github.com>
2025-11-29 22:14:21 +0100

    fix: add flathub repo via environment.etc instead of systemd service

--- modules/apps/core/flatpak.nix
+++ modules/apps/core/flatpak.nix
@@ -8,12 +8,11 @@
   services.flatpak.enable = true; # Flatpak
 
   # Add flathub repo automatically
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
+  environment.etc = {
+    "flatpak/remotes.d/flathub.flatpakrepo".source = pkgs.fetchurl {
+      url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      hash = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
+    };
   };
 
   system.fsPackages = [ pkgs.bindfs ];
