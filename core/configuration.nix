2467b1a8a0f5847f707688632c7206b5e56aaec9
kakxem <80788785+kakxem@users.noreply.github.com>
2025-11-29 13:54:49 +0100

    chore(core): remove nvidia drivers and packages

--- core/configuration.nix
+++ core/configuration.nix
@@ -4,7 +4,7 @@
 
 { config, pkgs, lib, user, ... }:
 
-{  
+{
   imports = [
     ./hardware-configuration.nix
     ../modules/desktops/gnome         # GNOME
@@ -40,29 +40,10 @@
   };
 
   # Hardware
-  hardware = {    
+  hardware = {
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-        libva
-      ];
-    };
-
-    nvidia = {
-      # Modesetting is needed for most wayland compositors
-      modesetting.enable = true;
-      powerManagement.enable = true;
-
-      # Use the open source version of the kernel module
-      open = false;
-
-      # Enable the nvidia settings menu
-      nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   };
 
@@ -78,8 +59,6 @@
       wireplumber.enable = true;
       audio.enable = true;
     };
-
-    xserver.videoDrivers = ["nvidia"];    # Nvidia drivers
 
     udev.packages = with pkgs; [
       via
@@ -126,26 +105,20 @@
   };
   services.pcscd.enable = true;
 
-  # Enable "UnFree" packages
-  nixpkgs.config.allowUnfree = true;
   environment = {
     systemPackages = with pkgs; [
       git
       neovim
       linuxKernel.packages.linux_zen.xone
       xclip
-      via
-      xdg-utils
     ];
 
     sessionVariables = {
       NIXOS_OZONE_WL = "1";             # Enable wayland for electron
-      
-      # Enable VA-API with nvidia for Firefox
-      MOZ_X11_EGL = "1";
+
+      # Enable VA-API for Firefox
       MOZ_DISABLE_RDD_SANDBOX = "1";
       EGL_PLATFORM = "wayland";
-      LIBVA_DRIVER_NAME = "nvidia";
       LIBVA_MESSAGING_LEVEL = "1";
     };
 
@@ -171,4 +144,3 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
-
