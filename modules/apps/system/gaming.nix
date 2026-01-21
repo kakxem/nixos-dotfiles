7d625bea437376a0d2a4b9e0a486b6015a403a03
kakxem <80788785+kakxem@users.noreply.github.com>
2026-01-21 22:22:24 +0100

    feat: enhance gaming setup and AMD GPU optimizations
    - Add lact, amdgpu_top, and gamescope to system packages
    - Enable MESA_ANTI_LAG environment variable
    - Configure gamemode with GPU optimizations and process priority
    - Enable LACT service for AMD GPU management

--- modules/apps/system/gaming.nix
+++ modules/apps/system/gaming.nix
@@ -11,12 +11,16 @@
     # Tools
     r2modman
     mangohud
+    lact
+    amdgpu_top
+    gamescope
   ];
 
   environment.variables = {
     PROTON_ENABLE_WAYLAND = 1;
     PROTON_ENABLE_HDR = 1;
     PROTON_FSR4_UPGRADE = 1;
+    ENABLE_LAYER_MESA_ANTI_LAG = 1;
   };
 
   programs = {
@@ -36,8 +40,21 @@
     # *Lutris* : General Preferences - Enable Feral GameMode - Global options - Add Environment Variables:
     # LD_PRELOAD=/nix/store/*-gamemode-*-lib/lib/libgamemodeauto.so
     #
-    gamemode.enable = true; # Better gaming performance
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          renice = 4;
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          amd_performance_level = "high";
+        };
+      };
+    };
   };
 
   hardware.xone.enable = true; # Xbox controller support
+  services.lact.enable = true; # Enable lact
 }
