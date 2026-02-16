7c091238c465f4599832b5d66c87af76297006a2
kakxem <80788785+kakxem@users.noreply.github.com>
2026-02-16 19:36:34 +0100

    feat(git): use gitFull and libsecret credential helper
    -Use pkgs.gitFull for Home Manager git
    -Set credential.helper=libsecret

--- modules/apps/home/terminal/git.nix
+++ modules/apps/home/terminal/git.nix
@@ -3,10 +3,12 @@
 {
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     settings = {
       user.name = gitName;
       user.email = gitEmail;
       commit.gpgsign = true;
+      credential.helper = "libsecret";
     };
     signing.key = gitKey;
   };
