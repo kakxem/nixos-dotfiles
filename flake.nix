2abf41bf6a032d946939c161bf35080b2782e9cd
kakxem <80788785+kakxem@users.noreply.github.com>
2026-06-27 23:22:00 +0200

    chore: remove nix-community cache
    - Drop nix-community Cachix substituter configuration.
    
    - Remove the matching trusted public key.

--- flake.nix
+++ flake.nix
@@ -5,9 +5,6 @@
   nixConfig = {
     # will be appended to the system-level substituters
     extra-substituters = [
-      # nix community's cache server
-      "https://nix-community.cachix.org"
-
       # Hyprland
       "https://hyprland.cachix.org"
 
@@ -23,9 +20,6 @@
 
     # will be appended to the system-level trusted-public-keys
     extra-trusted-public-keys = [
-      # nix community's cache server public key
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
       # Hyprland
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
 
