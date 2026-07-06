dd4020e38fe81c334fe34c57a3dade7bf5708bd4
kakxem <80788785+kakxem@users.noreply.github.com>
2026-07-06 20:07:12 +0200

    fix: update boot kernel configuration
    - Switch to linuxPackages_testing until zen catches up
    
    - Restore bzImage kernel file and add AMDGPU feature mask

--- modules/config/system/boot.nix
+++ modules/config/system/boot.nix
@@ -12,7 +12,8 @@
       efi.canTouchEfiVariables = true;
       timeout = 0; # Hide bootloader menu
     };
-    kernelPackages = pkgs.linuxPackages_zen;
+    # Stay on testing until linuxPackages_zen reaches Linux 7.2.
+    kernelPackages = pkgs.linuxPackages_testing;
 
     # Plymouth
     # Disabled because on high-end hardware (NVMe, last-gen GPU/CPU) the boot is too fast for Plymouth to be useful
@@ -31,11 +32,10 @@
       "udev.log_priority=3"
       "vt.global_cursor_default=0"
       "fbcon=nodefer"
+      "amdgpu.dcfeaturemask=1026"
     ];
   };
 
-  # Temporary workaround for linux_zen installing vmlinuz instead of bzImage:
-  # https://github.com/NixOS/nixpkgs/issues/535850
-  system.boot.loader.kernelFile = "vmlinuz";
+  system.boot.loader.kernelFile = "bzImage";
 
 }
