e708f9854de905b42d3bd82aaa8c8888fe5282eb
kakxem <80788785+kakxem@users.noreply.github.com>
2026-08-03 23:07:16 +0200

    fix: disable Sunshine automatic startup
    - Prevent the GDM user session from claiming Sunshine ports
    - Keep Sunshine available for manual startup

--- modules/apps/system/sunshine.nix
+++ modules/apps/system/sunshine.nix
@@ -7,7 +7,10 @@
 {
   services.sunshine = {
     enable = true;
-    autoStart = true;
+    # Disable the system-wide user unit because it also starts in GDM's greeter
+    # session and claims Sunshine's ports. Start Sunshine manually instead.
+    # https://github.com/nixos/nixpkgs/issues/513458
+    autoStart = false;
     capSysAdmin = true;
     openFirewall = true;
   };
